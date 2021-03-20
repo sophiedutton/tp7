f0cfe3289c9a3c48c8de2ecc7b21e55328fa29af
Sophie Dutton <sophie.dutton11@gmail.com>
2021-03-20 03:09:43 +0000

    tp7

--- scripts.js
+++ scripts.js
@@ -24,18 +24,50 @@ function loadFileInto(fromFile, whereTo) {
 
 	// now that everything is set, initiate request
 	ajax.send();
-
 }
 
-window.onload = function() {
-	
-	loadFileInto("ingredients.html", "ingredients");
-	
-	loadFileInto("equipment.html", "equipment");
-	
-	loadFileInto("directions.html", "directions");
-	
-};
+// object constructor for recipe prototype
+function Recipe(recipeName, imagesrc1, imagesrc2, contributorName, ingredientFilename, equipmentFilename, directionsFilename) {
+  this.name = recipeName;
+  this.imagesrc = imagesrc1;
+  this.imagesrc = imagesrc2;
+  this.contributor = contributorName;
+  this.ingFile = ingredientFilename;
+  this.equipFile = equipmentFilename;
+  this.dirFile = directionsFilename;
+  
+  //update the screen with this object's recipe information
+  this.displayRecipe = function() {
+    
+    //update the recipe title
+    document.querySelector("#titleBanner h1").innerHTML = this.name;
+    
+    //update the recipe contributor
+    document.querySelector("#titleBanner h3").innerHTML = "Contributed By: " + this.contributor;
+    
+    //update image 1
+    document.querySelector("#image1").style.backgroundImage = "url(" + this.imagesrc +")";
+    
+    //update image 1
+    document.querySelector("#image2").style.backgroundImage = "url(" + this.imagesrc +")";
+    
+    //update the 3 columns of information
+    loadFileInto(this.ingFile, "ingredients");
+    loadFileInto(this.equipFile, "equipment");
+    loadFileInto(this.dirFile, "directions");
+  }
+}
+
+LemonChickenPiccata = new Recipe(
+  "Lemon Chicken Piccata",
+  "chicken.jpg",
+  "chicken2.jpg",
+  "LemonLush",
+  "ingredients.html",
+  "equipment.html",
+  "directions.html"
+);
+
 
 headerClick = document.querySelector("#titleBanner h1");
 
@@ -63,3 +95,5 @@ x.innerHTML = "Hope you enjoy your meal!";
 x.style.fontSize = "50px";
 x.style.fontFamily = "Bebas Neue";
 x.style.textAlign = "center";
+
+
